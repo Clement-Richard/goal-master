13df927f973f4878c7f8e1685f9fa337a5bc3b2a
SkyDroxy <skydroxyjac@gmail.com>
2023-09-13 20:54:52 +0200

    Transformed Navbar to Sidebar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,90 +1,132 @@
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-zinc-900">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GoalMaster</span>
-      <button data-collapse-toggle="navbar-default" type="button"
-        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-default" aria-expanded="false"></button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-zinc-900 md:dark:bg-zinc-900 dark:border-zinc-700">
-          <li>
-            <nuxt-link to="/"
-              class="flex py-2 pl-3 pr-4 text-white bg-green-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
-              aria-current="page">
-              <span class="mr-1">
-                <icons.home class="navbar-link-icon"></icons.home>
-              </span>
-              Accueil
-            </nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/matchs"
-              class="flex py-2 pl-3 pr-4 text-white bg-green-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
-              aria-current="page">
-              <span class="mr-1">
-                <icons.bolt class="navbar-link-icon"></icons.bolt>
-              </span>
-              Matchs
-            </nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/profil"
-              class="flex py-2 pl-3 pr-4 text-white bg-green-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
-              aria-current="page">
-              <span class="mr-1">
-                <icons.user class="navbar-link-icon"></icons.user>
-              </span>
-              Profil
-            </nuxt-link>
-          </li>
-          <li>
-            <nuxt-link to="/contact"
-              class="flex py-2 pl-3 pr-4 text-white bg-green-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
-              aria-current="page">
-              <span class="mr-1">
-                <icons.envelope class="navbar-link-icon"></icons.envelope>
-              </span>
-              Contact</nuxt-link>
-          </li>
-          <li v-if="authStore.isLoggedIn">
-            <nuxt-link @click="logout" to="/"
-              class="flex py-2 pl-3 pr-4 text-white bg-green-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
-              aria-current="page">
-              <span class="mr-1">
-                <icons.doorLeave class="navbar-link-icon"></icons.doorLeave>
-              </span>
-              Déconnexion
-            </nuxt-link>
-          </li>
-          <li v-if="!authStore.isLoggedIn">
-            <nuxt-link to="/register"
-              class="flex py-2 pl-3 pr-4 text-white bg-green-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
-              aria-current="page">
-              <span class="mr-1">
-                <icons.lockClosed class="navbar-link-icon"></icons.lockClosed>
-              </span>
-              Inscription</nuxt-link>
-          </li>
-          <li v-if="!authStore.isLoggedIn">
-            <nuxt-link to="/login"
-              class="flex py-2 pl-3 pr-4 text-white bg-green-700 rounded md:bg-transparent md:text-emerald-500 md:p-0 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
-              aria-current="page">
-              <span class="mr-1">
-                <icons.lockOpened class="navbar-link-icon"></icons.lockOpened>
-              </span>
-              Connexion</nuxt-link>
-          </li>
-        </ul>
-      </div>
+  <aside
+    class="bg-zinc-900 text-white w-64 flex-shrink-0 h-screen fixed top-0 left-0 transform translate-x-0 transition-transform duration-300 ease-in-out flex flex-col"
+    :class="isOpen ? 'translate-x-0' : '-translate-x-48'"
+  >
+    <div class="p-4 ml-1 text-2xl font-semibold flex flex-row">
+      <img
+        src="~/assets/images/goalmaster-logo.png"
+        class="w-12 h-12 mr-2 ml-1"
+        :class="isOpen ? 'translate-x-0 w-full' : 'translate-x-44'"
+      />
+      <span class="mt-2" :class="isOpen ? '' : 'hidden'">Goal Master</span>
     </div>
-  </nav>
+    <ul class="justify-center font-medium p-4 mt-4 space-y-4">
+      <li>
+        <nuxt-link
+          to="/"
+          class="flex items-center py-2 ml-1 mr-1 px-4 w-full text-white bg-gradient-to-r from-emerald-700 to-emerald-500 rounded-lg hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-300 duration-300 hover:-translate-y-1 hover:scale-100"
+          :class="isOpen ? 'translate-x-0 w-full' : 'w-14 translate-x-44'"
+        >
+          <span class="mr-2"
+            ><icons.home class="navbar-link-icon"></icons.home
+          ></span>
+          <span :class="isOpen ? '' : 'hidden'">Accueil</span>
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          to="/matchs"
+          class="flex items-center py-2 ml-1 mr-1 px-4 w-full text-white bg-gradient-to-r from-emerald-700 to-emerald-500 rounded-lg hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-300 duration-300 hover:-translate-y-1 hover:scale-100"
+          :class="isOpen ? 'translate-x-0 w-full' : 'w-14 translate-x-44'"
+        >
+          <span class="mr-2"
+            ><icons.bolt class="navbar-link-icon"></icons.bolt
+          ></span>
+          <span :class="isOpen ? '' : 'hidden'">Matchs</span>
+        </nuxt-link>
+      </li>
+      <li v-if="authStore.isLoggedIn">
+        <nuxt-link
+          to="/profil"
+          class="flex items-center py-2 ml-1 mr-1 px-4 w-full text-white bg-gradient-to-r from-emerald-700 to-emerald-500 rounded-lg hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-300 duration-300 hover:-translate-y-1 hover:scale-100"
+          :class="isOpen ? 'translate-x-0 w-full' : 'w-14 translate-x-44'"
+        >
+          <span class="mr-2"><icons.user class="navbar-link-icon" /></span>
+          <span :class="isOpen ? '' : 'hidden'">Profil</span>
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link
+          to="/contact"
+          class="flex items-center py-2 ml-1 mr-1 px-4 w-full text-white bg-gradient-to-r from-emerald-700 to-emerald-500 rounded-lg hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-300 duration-300 hover:-translate-y-1 hover:scale-100"
+          :class="isOpen ? 'translate-x-0 w-full' : 'w-14 translate-x-44'"
+        >
+          <span class="mr-2"><icons.envelope class="navbar-link-icon" /></span>
+          <span :class="isOpen ? '' : 'hidden'">Contact</span>
+        </nuxt-link>
+      </li>
+      <li v-if="authStore.isLoggedIn">
+        <nuxt-link
+          @click="logout"
+          to="/"
+          class="flex items-center py-2 ml-1 mr-1 px-4 w-full text-white bg-gradient-to-r from-emerald-700 to-emerald-500 rounded-lg hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-300 duration-300 hover:-translate-y-1 hover:scale-100"
+          :class="isOpen ? 'translate-x-0 w-full' : 'w-14 translate-x-44'"
+        >
+          <span class="mr-2"><icons.doorLeave class="navbar-link-icon" /></span>
+          <span :class="isOpen ? '' : 'hidden'">Déconnexion</span>
+        </nuxt-link>
+      </li>
+      <li v-if="!authStore.isLoggedIn">
+        <nuxt-link
+          to="/register"
+          class="flex items-center py-2 ml-1 mr-1 px-4 w-full text-white bg-gradient-to-r from-emerald-700 to-emerald-500 rounded-lg hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-300 duration-300 hover:-translate-y-1 hover:scale-100"
+          :class="isOpen ? 'translate-x-0 w-full' : 'w-14 translate-x-44'"
+        >
+          <span class="mr-2"
+            ><icons.lockClosed class="navbar-link-icon"
+          /></span>
+          <span :class="isOpen ? '' : 'hidden'">Inscription</span>
+        </nuxt-link>
+      </li>
+      <li v-if="!authStore.isLoggedIn">
+        <nuxt-link
+          to="/register"
+          class="flex items-center py-2 ml-1 mr-1 px-4 w-full text-white bg-gradient-to-r from-emerald-700 to-emerald-500 rounded-lg hover:bg-gradient-to-r hover:from-emerald-600 hover:to-emerald-300 duration-300 hover:-translate-y-1 hover:scale-100"
+          :class="isOpen ? 'translate-x-0 w-full' : 'w-14 translate-x-44'"
+        >
+          <span class="mr-2"
+            ><icons.lockOpened class="navbar-link-icon"
+          /></span>
+          <span :class="isOpen ? '' : 'hidden'">Connexion</span>
+        </nuxt-link>
+      </li>
+    </ul>
+    <button
+      class="absolute bottom-0 right-0 p-4 bg-zinc-900 text-white"
+      @click="toggleSidebar"
+    >
+      <icons.arrowCircleLeft
+        class="w-8 h-8 hover:scale-125"
+        :class="
+          isOpen
+            ? 'transition-transform duration-300'
+            : 'transition-transform rotate-180 duration-300'
+        "
+      ></icons.arrowCircleLeft>
+    </button>
+  </aside>
 </template>
 
 <script setup>
 import { useAuthStore } from "~/store/auth";
 
-import { HomeIcon, UserIcon, BoltIcon, EnvelopeIcon, LockClosedIcon, LockOpenIcon, ArrowRightOnRectangleIcon } from "@heroicons/vue/24/solid";
+import {
+  HomeIcon,
+  UserIcon,
+  BoltIcon,
+  EnvelopeIcon,
+  LockClosedIcon,
+  LockOpenIcon,
+  ArrowRightOnRectangleIcon,
+  ArrowLeftCircleIcon,
+} from "@heroicons/vue/24/solid";
+
+const isOpen = ref(true);
+
+const toggleSidebar = () => {
+  isOpen.value = !isOpen.value;
+};
 
 const icons = reactive({
   home: HomeIcon,
@@ -93,7 +135,8 @@ const icons = reactive({
   envelope: EnvelopeIcon,
   lockClosed: LockClosedIcon,
   lockOpened: LockOpenIcon,
-  doorLeave: ArrowRightOnRectangleIcon
+  doorLeave: ArrowRightOnRectangleIcon,
+  arrowCircleLeft: ArrowLeftCircleIcon,
 });
 
 const authStore = useAuthStore();
