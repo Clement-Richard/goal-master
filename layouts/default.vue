d66a72ecc0a82895f6af6541ad0a02816a0818e1
SkyDroxy <skydroxyjac@gmail.com>
2023-09-13 22:12:50 +0200

    Replace Navbar By Sidebar & Update Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,17 @@
+import { Navbar } from '../.nuxt/components';
 <template>
   <div>
-    <Navbar />
-    <slot />
+    <Sidebar class="z-50" :sidebarIsOpened="sidebarIsOpened" @toggle-sidebar="toggleSidebar"/>
+    <div class="transition duration-300" id="content">
+      <slot />
+    </div>
   </div>
 </template>
 
 <script setup>
+  const sidebarIsOpened = ref(false)
+
+  const toggleSidebar = () => {
+    sidebarIsOpened.value = !sidebarIsOpened.value ;
+  };
 </script>
